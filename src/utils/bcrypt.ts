20ebb43e1902b41cbe29c3e214c25bda80a7778e
Tyander Boldt <tyandrer.boldt@gmail.com>
2024-10-01 20:19:24 -0300

    Add bcrypt

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 
-export async function hashPass(pass: string) {
-  return bcrypt.hash(pass, 10) as Promise<string>;
+export async function hashPass(unHashPass: string) {
+  return bcrypt.hash(unHashPass, 10) as Promise<string>;
 }
 
 export async function isSamePass(unHashPass: string, hashPass: string) {
